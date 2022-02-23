e73e7f6fd6668c41fb5f2edf3d46ce9bb6dc2f1c
Arpit <arpitmh23@gmail.com>
2022-02-23 15:18:32 +0530

    Removed unrequired commented part and updated  methods and commented it

--- src/main/java/com/bridgelabz/addressbookapplication/service/IAddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapplication/service/IAddressBookService.java
@@ -4,23 +4,17 @@ import com.bridgelabz.addressbookapplication.dto.AddressBookDTO;
 import com.bridgelabz.addressbookapplication.model.Address;
 
 import java.util.List;
-import java.util.Optional;
 
+//Created interface for all service methods to achieve abstraction
 public interface IAddressBookService {
-    public String getMessage(String name);
-
-    public String postMessage(Address address);
-
-    public String putMessage(String name);
 
     public String getWelcome();
 
     public Address saveDataToRepo(AddressBookDTO addressBookDTO);
 
-    public Optional<Address> getRecordById(Integer id);
+    public Address getRecordById(Integer id);
 
     public List<Address> getRecord();
-
 
     public Address updateRecordById(Integer id, AddressBookDTO addressBookDTO);
 
